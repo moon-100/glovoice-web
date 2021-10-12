5b878402174622408cb9bc5838fddad8dfdf7715
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-10-12 10:57:47 +0900

    Add : NavPage 기본 레이아웃 완료

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+import NavSide from './NavSide';
 
 const Nav = () => {
-  return <div>nav</div>;
+  return (
+    <Container>
+      <TopContainer>
+        <TopHeader>BI</TopHeader>
+        <TopSignBtn>Logout</TopSignBtn>
+      </TopContainer>
+      <SideContainer>
+        <NavSide />
+      </SideContainer>
+    </Container>
+  );
 };
-
+const Container = styled.div``;
+const TopContainer = styled.div`
+  position: sticky;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  border-bottom: 2px solid rgb(230, 230, 230);
+`;
+const TopHeader = styled.div`
+  width: 200px;
+  text-align: center;
+  line-height: 60px;
+  background-color: rgb(239, 239, 239);
+  font-size: 20px;
+`;
+const TopSignBtn = styled.button`
+  width: 100px;
+  border: none;
+  background-color: rgb(239, 239, 239);
+  font-size: 18px;
+`;
+const SideContainer = styled.div`
+  position: fixed;
+  top: 60px;
+  left: 0;
+  width: 200px;
+  height: 100%;
+  border-right: 2px solid rgb(230, 230, 230);
+`;
 export default Nav;
--- src/components/Nav/NavSide.tsx
+++ src/components/Nav/NavSide.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ArrowIosDownwardOutline } from '@styled-icons/evaicons-outline/ArrowIosDownwardOutline';
 
 const NavSide = () => {
-  return <div>navside</div>;
+  return (
+    <>
+      <SideNav>
+        <SideNavHeader>
+          클라이언트 관리
+          <ArrowDownIcon />
+        </SideNavHeader>
+        <SideNavContentBox>
+          <SideNavContent>클라이언트 현황</SideNavContent>
+        </SideNavContentBox>
+      </SideNav>
+      <SideNav>
+        <SideNavHeader>
+          사용자 관리
+          <ArrowDownIcon />
+        </SideNavHeader>
+        <SideNavContentBox>
+          <SideNavContent>사용자 현황</SideNavContent>
+        </SideNavContentBox>
+      </SideNav>
+      <SideNav>
+        <SideNavHeader>
+          소싱 관리
+          <ArrowDownIcon />
+        </SideNavHeader>
+        <SideNavContentBox>
+          <SideNavContent>소싱 파일 관리</SideNavContent>
+        </SideNavContentBox>
+        <SideNavContentBox>
+          <SideNavContent>소싱 현황 관리</SideNavContent>
+        </SideNavContentBox>
+      </SideNav>
+    </>
+  );
 };
-
+const SideNav = styled.div`
+  width: 100%;
+  border-bottom: 2px solid rgb(211, 211, 211);
+`;
+const SideNavHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  line-height: 60px;
+  margin: 0px 10px;
+`;
+const SideNavContentBox = styled.div`
+  margin-left: 50px;
+  border-top: 2px solid rgb(211, 211, 211);
+`;
+const SideNavContent = styled.div`
+  height: 60px;
+  line-height: 60px;
+  margin-left: -10px;
+`;
+const ArrowDownIcon = styled(ArrowIosDownwardOutline)`
+  width: 23px;
+`;
 export default NavSide;
