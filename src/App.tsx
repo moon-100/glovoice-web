e3dcfeb8432aeffd5c7a231a0ac18ffb29228682
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-10-12 10:59:58 +0900

    Fix : AdminLoginPage 수정중

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Nav from 'components/Nav/Nav';
+import AdminSingin from 'pages/adminSignin/AdminSingin';
 
 const App = () => {
-  return <Nav />;
+  return <AdminSingin />;
 };
 
 export default App;
