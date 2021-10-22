07e4db5304822c56de00ac3f491afedad2f8c294
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-10-22 09:45:42 +0900

    [Add] user page - 유저 리스트 페이지 추가

--- src/pages/AdminMain/UserList/UserTable.tsx
+++ src/pages/AdminMain/UserList/UserTable.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import DetailBtn from 'components/Button/DetailBtn';
+import { ArrowIosDownwardOutline } from '@styled-icons/evaicons-outline/ArrowIosDownwardOutline';
+import { ArrowIosUpwardOutline } from '@styled-icons/evaicons-outline/ArrowIosUpwardOutline';
 
 interface Iprops {
   userTable: {
-    Num: string;
+    Num: string | number;
     Id: string;
     FirstName: string;
     MiddleName: string;
@@ -11,13 +14,29 @@ interface Iprops {
     Gender: string;
     Birthday: string;
     Language: string;
-    RegDate: string;
-    Detail: string;
-    Status: string;
+    RegDate?: string;
+    Detail?: string;
+    Status?: string | boolean;
   };
+  setUserId: React.Dispatch<React.SetStateAction<number>>;
+  setPage: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const UserTable = ({ userTable }: Iprops) => {
+const UserTable = ({ userTable, setUserId, setPage }: Iprops) => {
+  const [activeWindow, setActiveWindow] = useState(false);
+
+  const clickWindowHandler = () => {
+    setActiveWindow(!activeWindow);
+  };
+
+  const conditionHandler = (value: boolean) => {
+    if (value) {
+      console.log('활성화');
+    } else {
+      console.log('비활성화');
+    }
+  };
+
   return (
     <>
       <Num>{userTable.Num}</Num>
@@ -29,8 +48,50 @@ const UserTable = ({ userTable }: Iprops) => {
       <Birthday>{userTable.Birthday}</Birthday>
       <Language>{userTable.Language}</Language>
       <RegDate>{userTable.RegDate}</RegDate>
-      <Detail>{userTable.Detail}</Detail>
-      <Status>{userTable.Status}</Status>
+      <Detail>
+        {typeof userTable.Num === 'number' ? (
+          <DetailBtn
+            id={userTable.Num}
+            page="userDetail"
+            setId={setUserId}
+            setPage={setPage}
+          />
+        ) : (
+          userTable.Detail
+        )}
+      </Detail>
+      {typeof userTable.Status === 'boolean' ? (
+        <Status>
+          <FilterBox>
+            <FilterTextBox onClick={clickWindowHandler}>
+              {userTable.Status ? (
+                <ActiveText>Activated</ActiveText>
+              ) : (
+                <DeactiveText>Deactivated</DeactiveText>
+              )}
+              {activeWindow ? <ArrowUpIcon /> : <ArrowDownIcon />}
+            </FilterTextBox>
+            {activeWindow && (
+              <FilterNavBox>
+                <FilterNav
+                  onClick={() => conditionHandler(true)}
+                  style={{ color: 'blue', fontWeight: 'bold' }}
+                >
+                  Activated
+                </FilterNav>
+                <FilterNav
+                  onClick={() => conditionHandler(false)}
+                  style={{ color: 'red', fontWeight: 'bold' }}
+                >
+                  Deactivated
+                </FilterNav>
+              </FilterNavBox>
+            )}
+          </FilterBox>
+        </Status>
+      ) : (
+        <Status>{userTable.Status}</Status>
+      )}
     </>
   );
 };
@@ -88,7 +149,7 @@ const Language = styled.li`
 `;
 
 const RegDate = styled.li`
-  width: 15%;
+  width: 12%;
   text-align: center;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -102,10 +163,70 @@ const Detail = styled.li`
 `;
 
 const Status = styled.li`
-  width: 10%;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  width: 13%;
   overflow: hidden;
   text-overflow: ellipsis;
+`;
+
+const FilterBox = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  margin-right: 10px;
+  margin-top: -15px;
+`;
+
+const FilterTextBox = styled.div`
+  display: flex;
+  align-items: center;
+  height: 30px;
+  border: 1px solid rgb(220, 220, 220);
+`;
+
+const ActiveText = styled.div`
+  padding: 5px;
+  color: blue;
+  font-size: 13px;
+  font-weight: bold;
+`;
+
+const DeactiveText = styled.div`
+  padding: 5px;
+  color: red;
+  font-size: 13px;
+  font-weight: bold;
+`;
+
+const FilterNavBox = styled.div`
+  position: absolute;
+  top: 30px;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: white;
+`;
+
+const FilterNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 30px;
+  margin-top: -1px;
+  padding: 5px;
+  border: 1px solid rgb(220, 220, 220);
+  font-size: 13px;
+`;
+
+const ArrowDownIcon = styled(ArrowIosDownwardOutline)`
+  width: 15px;
+  margin: 0 10px;
+`;
+
+const ArrowUpIcon = styled(ArrowIosUpwardOutline)`
+  width: 15px;
+  margin: 0 10px;
 `;
 
 export default UserTable;
