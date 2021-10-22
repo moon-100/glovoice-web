e715de67d9137088efea1a906438a2371e3056fd
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-10-22 18:14:25 +0900

    [Mod] login page - 레이아웃 수정 및 기능 추가

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL = `${process.env.REACT_APP_SERVER_URL}`;
-export const SIGN_IN = `${BASE_URL}/client/login`;
+export const SIGN_IN = `${BASE_URL}/admin/client/login`;
 export const SIGN_UP = `${BASE_URL}/client/signup`;
 export const DELETE_CLIENT = `${BASE_URL}/client`;
 export const PASSWORD_INIT = `${BASE_URL}/client/detail/passphrases`;
--- src/pages/AdminSignin/AdminSignin.tsx
+++ src/pages/AdminSignin/AdminSignin.tsx
@@ -3,18 +3,21 @@ import styled from 'styled-components';
 import { useHistory } from 'react-router';
 import { SIGN_IN } from 'config';
 
+interface loginErrorType {
+  readonly loginError?: boolean;
+}
+
 const AdminSignin = () => {
-  const [email, setEmail] = useState('');
+  const history = useHistory();
+  const [loginId, setLoginId] = useState('');
   const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(false);
 
   const signin = () => {
-    const history = useHistory();
     fetch(`${SIGN_IN}`, {
       method: 'POST',
-      body: JSON.stringify({
-        email: { email },
-        password: { password },
-      }),
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ loginId, password }),
     })
       .then((res) => res.json())
       .then((res) => {
@@ -23,6 +26,7 @@ const AdminSignin = () => {
           history.push('/main');
         } else {
           alert(`Please check your account.`);
+          setLoginError(true);
         }
       });
   };
@@ -30,15 +34,17 @@ const AdminSignin = () => {
   return (
     <Container>
       <ContentFrame>
-        <Logo alt="Logo" src="/images/logo_glo_voice.png" />
+        <Logo alt="Logo" src="/images/logo-glo-voice.png" />
         <InputContainer>
-          <InputText>Username</InputText>
+          <InputText>ID</InputText>
           <InputBox
             type="text"
-            placeholder="Your Email"
+            placeholder="Your ID"
             onChange={(e) => {
-              setEmail(e.target.value);
+              setLoginId(e.target.value);
             }}
+            loginError={loginError}
+            defaultValue={loginId}
           />
           <InputText>Password</InputText>
           <InputBox
@@ -47,7 +53,10 @@ const AdminSignin = () => {
             onChange={(e) => {
               setPassword(e.target.value);
             }}
+            loginError={loginError}
+            defaultValue={password}
           />
+          {loginError && <ErrorBox>Please check your account.</ErrorBox>}
         </InputContainer>
         <SigninBtn onClick={signin}>LOG IN</SigninBtn>
       </ContentFrame>
@@ -66,44 +75,68 @@ const ContentFrame = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 420px;
+  height: 338px;
 `;
 
 const Logo = styled.img`
-  width: 150px;
+  width: 236px;
+  height: 38px;
   margin-bottom: 20px;
 `;
 
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 15px;
+  margin-bottom: auto;
 `;
 
 const InputText = styled.div`
-  margin-top: 20px;
-  font-size: 15px;
+  height: 20px;
+  margin-bottom: 8px;
+  font-size: 14px;
   font-weight: bold;
+  font-family: SpoqaHanSans;
 `;
 
-const InputBox = styled.input`
-  width: 350px;
-  height: 45px;
-  margin: 10px 0;
-  padding-left: 20px;
-  border: 2px solid rgb(159, 183, 246);
-  border-radius: 5px;
-  font-size: 12px;
+const InputBox = styled.input<loginErrorType>`
+  width: 328px;
+  height: 48px;
+  margin-bottom: 16px;
+  padding: 13px 8px 15px 8px;
+  border: none;
+  border-radius: 8px;
+  background-color: #f5f5f5;
+  font-size: 14px;
+  font-family: SpoqaHanSans;
+
+  /* ${({ loginError }) =>
+    loginError ? `background-color:blue;` : `background-color:red;`} */
+
+  ${({ loginError }) =>
+    loginError && `border:1px solid #e44646; background-color:#fff2f2;`}
+
+  ::placeholder {
+    color: #111;
+  }
+`;
+
+const ErrorBox = styled.div`
+  width: 100%;
+  margin-top: -8px;
+  font-size: 14px;
+  text-align: center;
+  color: #e44646;
 `;
 
 const SigninBtn = styled.button`
-  width: 250px;
-  height: 40px;
-  margin-top: 20px;
-  border-radius: 20px;
-  color: white;
-  background-color: ${({ theme }) => theme.color.blue};
+  width: 328px;
+  height: 48px;
+  border-radius: 24px;
+  color: rgba(255, 255, 255, 0.47);
   border: none;
-  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  background-color: #111;
+  font-family: SpoqaHanSans;
 `;
 
 export default AdminSignin;
